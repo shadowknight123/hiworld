1842d41b66c03ff81060f8ba76641222f1c9e8f3
wong1998 <2366648221@qq.com>
2020-09-26 21:58:30 +0800

    test

--- src/test.java
+++ src/test.java
@@ -5,7 +5,7 @@ public class test {
     public static void main(String[] args){
         int []a = new int[10];
         Scanner in = new Scanner(System.in);
-        System.out.println("What is your name?");
+        System.out.println("What is your ?");
         String name = in.nextLine();
         System.out.println(name);
     }
